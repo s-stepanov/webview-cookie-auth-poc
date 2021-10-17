5ae8b8cc85fbf60ae0241509bbeb4b853a05d206
Sergey Stepanov <sstepanov@lineate.com>
2021-10-17 17:41:36 +0600

    add webview auth poc

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{
+        uri: 'http://localhost:5000/api/auth/mobile-platform-redirect?source=http://localhost:5000/&remember=false',
+        headers: {
+          // Authorization: 'Bearer ', - ADD REFRESH TOKEN HERE
+        },
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
